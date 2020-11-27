779522e4e2d5bd3e95852fb66860b29f52998fea
macebm <marcelbauerat@gmail.com>
2020-11-27 12:58:42 +0100

    Commands deleted

--- backend/src/Repository/StoreRepository.php
+++ backend/src/Repository/StoreRepository.php
@@ -37,33 +37,4 @@ class StoreRepository extends ServiceEntityRepository
 
         return $store;
     }
-
-    // /**
-    //  * @return Store[] Returns an array of Store objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('s')
-            ->andWhere('s.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('s.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Store
-    {
-        return $this->createQueryBuilder('s')
-            ->andWhere('s.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
